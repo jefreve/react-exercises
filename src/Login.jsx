556de60e1111678edfe2ae6b49cd78cf3c61debc
Jef <jefersonrevellobiz@gmail.com>
2023-09-11 13:37:05 +0200

    working on exercise

---
+++ src/Login.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+  };
+
+  handleChangeInput = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleResetInputs = (event) => {
+    this.setState({ username: "", password: "" });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          name="username"
+          type="text"
+          value={this.state.username}
+          onChange={this.handleChangeInput}
+        />
+        <input
+          name="password"
+          type="password"
+          value={this.state.password}
+          onChange={this.handleChangeInput}
+        />
+        <button
+          disabled={
+            this.state.username.length > 0 && this.state.password.length > 0
+              ? false
+              : true
+          }
+          onClick={this.props.loginFunction}
+        >
+          Login
+        </button>
+        <button onClick={this.handleResetInputs}>Reset</button>
+      </div>
+    );
+  }
+}
