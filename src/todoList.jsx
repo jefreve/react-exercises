ee68c53ffd8748d8ef91e0d9e6abb26c47ea7ac6
Jef <jefersonrevellobiz@gmail.com>
2023-09-12 21:36:22 +0200

    add exercise

---
+++ src/todoList.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+export class TodoList extends React.Component {
+  state = {
+    items: ["Homework", "Project", "Night out", "Workout", "Play guitar"],
+  };
+
+  addTodo = (event) => {
+    const input = event.target.parentNode.querySelector('[name="inputField"]');
+    const inputValue = input.value;
+
+    this.clearTodo(input);
+
+    const newItemsArray = this.state.items;
+
+    newItemsArray.push(inputValue);
+
+    this.setState({ items: newItemsArray });
+  };
+
+  clearTodo = (input) => {
+    input.value = "";
+  };
+
+  resetItems = () => {
+    this.setState({ items: [] });
+  };
+
+  removeItem = (event) => {
+    const index = event.target.name;
+    const newArray = this.state.items;
+
+    newArray.splice(index, 1);
+
+    this.setState({ items: newArray });
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.items.map((todo, index) => (
+            <li key={todo.value + "-" + index}>
+              {`Todo N.${index + 1}: ${todo}`}
+              <button name={index} onClick={this.removeItem}>
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+        <input name="inputField" type="text" />
+        <button onClick={this.addTodo}>Add Todo</button>
+        <button onClick={this.resetItems}>Reset</button>
+      </div>
+    );
+  }
+}
