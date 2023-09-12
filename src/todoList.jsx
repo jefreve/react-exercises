5eccb5ef074d6979a618d9238e52b98dc53e1042
Jef <jefersonrevellobiz@gmail.com>
2023-09-12 18:47:35 +0200

    add exercise

---
+++ src/todoList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export class TodoList extends React.Component {
+  state = {
+    items: ["Homework", "Project", "Night out", "Workout", "Play guitar"],
+  };
+
+  addTodo = (event) => {
+    const value = event.target.parentNode.querySelector("input").value;
+    const newItemsArray = this.state.items;
+    newItemsArray.push(value);
+    this.setState({ items: newItemsArray });
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.items.map((todo, index) => (
+            <li key={todo.value + "-" + index}>{`Todo N.${
+              index + 1
+            }: ${todo}`}</li>
+          ))}
+        </ul>
+        <input name="inputField" type="text" />
+        <button onClick={this.addTodo}>Add Todo</button>
+      </div>
+    );
+  }
+}
