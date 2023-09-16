b45fe6b53a59b24b20ce55a2822e04c3f69535f8
Jef <jefersonrevellobiz@gmail.com>
2023-09-16 18:48:04 +0200

    add exercise

---
+++ QUESTION.md
@@ -0,0 +1,3 @@
+Modify the UncontrolledLogin component so that the username input
+
+is automatically focused when the component renders the first time.
---
+++ src/UncontrolledLogin.jsx
@@ -0,0 +1,74 @@
+import React, { createRef } from "react";
+
+export class UncontrolledLogin extends React.Component {
+  _formRef = createRef();
+
+  constructor(props) {
+    super(props);
+
+    this.disabledInitialValue = true;
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const username = this._formRef.current.username.value;
+    const password = this._formRef.current.password.value;
+
+    console.log({
+      username,
+      password,
+    });
+  };
+
+  handleChangeInput = () => {
+    const user = this._formRef.current.username.value;
+    const pass = this._formRef.current.password.value;
+    let loginBtn = this._formRef.current.loginBtn;
+    const expression = user.length > 0 && pass.length > 0;
+
+    loginBtn.disabled = expression ? false : true;
+  };
+
+  handleReset = () => {
+    const username = this._formRef.current.username;
+    const password = this._formRef.current.password;
+    username.value = "";
+    password.value = "";
+
+    let loginBtn = this._formRef.current.loginBtn;
+    loginBtn.disabled = true;
+  };
+
+  componentDidMount() {
+    this._formRef.current.username.focus();
+  }
+
+  render() {
+    return (
+      <div>
+        <form ref={this._formRef} onSubmit={this.handleSubmit}>
+          <input name="username" onChange={this.handleChangeInput} />
+          <input
+            name="password"
+            type="password"
+            onChange={this.handleChangeInput}
+            autoComplete="username"
+          />
+
+          <button
+            disabled={this.disabledInitialValue}
+            type="submit"
+            name="loginBtn"
+            autoComplete="current-password"
+          >
+            Login
+          </button>
+          <button type="button" onClick={this.handleReset}>
+            Reset
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
