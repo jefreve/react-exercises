a158e9e67f48d14219fac80725fc5b45a92b7a27
Jef <jefersonrevellobiz@gmail.com>
2023-09-20 14:59:01 +0200

    edit exercise - 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Login } from "./Login";
-import { Login2 } from "./Login2";
 
 export function App() {
   return (
     <div>
-      <Login2 />
+      <Login />
     </div>
   );
 }
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,57 +1,54 @@
 import React, { useState } from "react";
 
 export function Login() {
-  const [data, setData] = useState({
+  const [user, setUser] = useState({
     username: "",
     password: "",
     remember: false,
   });
 
-  function handleLogin(event) {
-    event.preventDefault();
-    const username = event.target.parentElement.children.username.value;
-    const password = event.target.parentElement.children.password.value;
-    const remember = event.target.parentElement.children.remember.checked;
+  function handleInput(event) {
+    const { name, type, value, checked } = event.target;
 
-    const obj = { username: username, password: password, remember: remember };
-    console.log(obj);
+    setUser((prevData) => ({
+      ...prevData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
   }
 
-  function handleInputChange(event) {
-    const target = event.target;
-    const { name, value, type, checked } = target;
-
-    setData((data) => {
-      return { ...data, [name]: type === "checkbox" ? checked : value };
-    });
+  function handleLogin() {
+    console.log(user);
   }
 
   function handleReset() {
-    setData({ username: "", password: "", remember: false });
+    setUser({
+      username: "",
+      password: "",
+      remember: false,
+    });
   }
 
   return (
-    <form>
-      <input
-        name="username"
-        onChange={handleInputChange}
-        value={data.username}
-      />
+    <div>
+      <input name="username" value={user.username} onChange={handleInput} />
       <input
         name="password"
         type="password"
-        onChange={handleInputChange}
-        value={data.password}
+        value={user.password}
+        onChange={handleInput}
       />
       <input
         name="remember"
         type="checkbox"
-        onChange={handleInputChange}
-        checked={data.remember}
+        checked={user.remember}
+        onChange={handleInput}
       />
-
-      <button onClick={handleLogin}>Login</button>
+      {/* if either username or password is an empty string, 
+      Login button is disabled */}
+      <button onClick={handleLogin} disabled={!user.username || !user.password}>
+        Login
+      </button>
       <button onClick={handleReset}>Reset</button>
-    </form>
+    </div>
   );
 }
