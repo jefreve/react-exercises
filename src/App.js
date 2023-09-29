c9702bccda323f0aeddc717af07a13ea3aeff2d1
Jef <jefersonrevellobiz@gmail.com>
2023-09-29 12:57:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+
+export function App() {
+  return (
+    <div>
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  );
+}
